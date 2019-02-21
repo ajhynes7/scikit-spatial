53f7226e636b8f70269626c804dbec19c357f7d1
Andrew Hynes <andrewhynes@ymail.com>
2019-02-21 15:51:25 -0500

    Add property tests for Line

--- tests/property/test_objects.py
+++ tests/property/test_objects.py
@@ -1,10 +1,12 @@
+"""Test properties of spatial objects."""
+
 import hypothesis.strategies as st
 import numpy as np
 import pytest
 from hypothesis import given
 from numpy.testing import assert_array_equal, assert_allclose
 
-from skspatial import Point, Vector
+from skspatial import Point, Vector, Line
 
 
 # Absolute tolerance for np.isclose and np.allclose functions.
@@ -26,9 +28,9 @@ def test_length(array):
 
     if len(array) > 3:
         with pytest.raises(Exception):
-            point = Point(array)
+            Point(array)
         with pytest.raises(Exception):
-            vector = Vector(array)
+            Vector(array)
     else:
 
         point = Point(array)
@@ -49,3 +51,21 @@ def test_unit_vector(array):
 
     assert np.isclose(unit_vector.magnitude, 1)
     assert_allclose(vector.magnitude * unit_vector.array, vector.array)
+
+
+@given(st_arrays_allowed, st_arrays_allowed)
+def test_line(array_1, array_2):
+
+    point_1 = Point(array_1)
+    vector = Vector(array_2)
+
+    point_2 = point_1.add(vector)
+
+    line_1 = Line(point_1, vector)
+    line_2 = Line.from_points(point_1, point_2)
+
+    assert line_1.is_close(line_2)
+
+    # A point and vector are not interchangeable.
+    with pytest.raises(Exception):
+        Line(vector, point_1)
