f541f584e5442fa92ae58bb4804baceb10c2c3a3
Andrew Hynes <andrewhynes@ymail.com>
2019-03-04 16:33:59 -0500

    Refactor class name in test_arrays

--- tests/unit/test_arrays.py
+++ tests/unit/test_arrays.py
@@ -29,12 +29,12 @@ def test_equality(point_a, point_b):
     assert not point_b == vector_b
 
 
-@pytest.mark.parametrize("class_", [Point, Vector])
-def test_length(class_):
+@pytest.mark.parametrize("PointOrVector", [Point, Vector])
+def test_length(PointOrVector):
     """The output point/vector is always 3D."""
-    object_1 = class_([1])
-    object_2 = class_([1, 1])
-    object_3 = class_([1, 1, 1])
+    object_1 = PointOrVector([1])
+    object_2 = PointOrVector([1, 1])
+    object_3 = PointOrVector([1, 1, 1])
 
     assert all(x.array.size == 3 for x in [object_1, object_2, object_3])
 
