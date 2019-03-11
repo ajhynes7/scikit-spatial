0f2c3c1f93b0aed935edb39ad2aa11b07b331a96
Andrew Hynes <andrewhynes@ymail.com>
2019-03-11 08:06:40 -0400

    Update line and plane to use new Vector class

--- skspatial/objects/base_line_plane.py
+++ skspatial/objects/base_line_plane.py
@@ -1,24 +1,31 @@
 """Classes for the Line and Plane spatial objects."""
 
 import numpy as np
-from dpcontracts import require, ensure, types
+from dpcontracts import require, ensure
 
-from .array_objects import Point, Vector
+from skspatial.objects.point import Point
+from skspatial.objects.vector import Vector
 
 
 class _BaseLinePlane:
     """Private parent class for Line and Plane."""
 
-    @types(point=Point, vector=Vector)
-    @require("The vector cannot be the zero vector.", lambda args: not args.vector.is_zero())
-    def __init__(self, point, vector):
+    @require("The vector cannot be the zero vector.", lambda args: not Vector(args.vector).is_zero())
+    @ensure("The point is a Point.", lambda args, _: isinstance(args.self.point, Point))
+    @ensure("The vector is a Vector", lambda args, _: isinstance(args.self.vector, Vector))
+    def __init__(self, point=[0, 0, 0], vector=[1, 0, 0]):
 
-        self.point = point
-        self.vector = vector.unit()
+        self.point = Point(point)
+        self.vector = Vector(vector).unit()
 
-    def __eq__(self, other):
+    def __repr__(self):
 
-        return vars(self) == vars(other)
+        name_class = type(self).__name__
+
+        repr_point = np.array_repr(self.point)
+        repr_vector = np.array_repr(self.vector)
+
+        return f"{name_class}(point={repr_point}, normal={repr_vector})"
 
     @require("The input must have the same type as the object.", lambda args: isinstance(args.other, type(args.self)))
     def is_close(self, other, **kwargs):
@@ -28,7 +35,6 @@ class _BaseLinePlane:
 
         return close_point and close_vector
 
-    @types(point=Point)
     @ensure("The output must zero or greater.", lambda _, result: result >= 0)
     @ensure("The output must be a numpy scalar.", lambda _, result: isinstance(result, np.number))
     def distance_point(self, point):
