1c17e83072810b81342511306829b7f964f886e3
Andrew Hynes <andrewhynes@ymail.com>
2019-03-15 09:59:02 -0400

    Update test_array_objects

--- tests/unit/objects/test_array_objects.py
+++ tests/unit/objects/test_array_objects.py
@@ -8,43 +8,26 @@ from skspatial.objects import Point, Vector
 
 
 @pytest.mark.parametrize(
-    "point_a, point_b",
+    "array",
     [
-        (Point([1]), Point([1])),
-        (Point([1]), Point([1, 0])),
-        (Point([1]), Point([1, 0, 0])),
-        (Point([1, 0]), Point((1, 0))),
-        (Point([1, 0]), Point(np.array([1, 0]))),
+        [1],
+        [1, 2],
+        [1, 2, 3],
+        [1, 2, 3, 4],
+        [1, 2, 3, 4, 5],
     ],
 )
-def test_equality(point_a, point_b):
+def test_equality(array):
 
-    assert_array_equal(point_a, point_b)
-
-    vector_a = Vector(point_a)
-    vector_b = Vector(point_b)
-
-    assert_array_equal(vector_a, vector_b)
-
-    assert_array_equal(point_a, vector_a)
-    assert_array_equal(point_b, vector_b)
-
-
-@pytest.mark.parametrize("class_spatial", [Point, Vector])
-def test_length(class_spatial):
-    """The output point/vector is always 3D."""
-    object_1 = class_spatial([1])
-    object_2 = class_spatial([1, 1])
-    object_3 = class_spatial([1, 1, 1])
-
-    assert all(x.size == 3 for x in [object_1, object_2, object_3])
+    assert_array_equal(array, Point(array))
+    assert_array_equal(array, Vector(array))
+    assert_array_equal(array, np.array(array))
 
 
 @pytest.mark.parametrize(
     "array",
     [
         [],
-        [1, 1, 1, 1],
         [np.nan],
         [1, 1, np.nan],
         [1, 1, np.inf],
