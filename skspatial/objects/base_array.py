a818c1afbbb8e251e998b0a7e61f54785401f8d1
Andrew Hynes <andrewhynes@ymail.com>
2019-03-15 20:07:39 -0400

    Add Points class

--- skspatial/objects/base_array.py
+++ skspatial/objects/base_array.py
@@ -30,3 +30,34 @@ class _BaseArray1D(np.ndarray):
     def is_close(self, other, **kwargs):
         """Check if array is close to another array."""
         return np.allclose(self, _BaseArray1D(other), **kwargs)
+class _BaseArray2D(np.ndarray):
+    """Private base class for spatial objects based on a single 2D NumPy array."""
+
+    @require("The input array must only contain finite numbers.", lambda args: np.all(np.isfinite(args.array_like)))
+    def __new__(cls, array_like):
+
+        # We cast the input array to be our class type.
+        obj = np.asarray(array_like).view(cls)
+
+        return obj
+
+    def __array_finalize__(self, obj):
+
+        if obj is None:
+            return
+
+    @require(
+        "The points dimension cannot be greater than the desired dimension.", lambda args: args.dim > args.self.shape[1],
+    )
+    @ensure(
+        "The output must have the desired dimensions.", lambda args, result: result.shape == (len(args.self), args.dim),
+    )
+    def set_dimension(self, dim):
+        """
+        Change the dimension of the points.
+
+        """
+        n_rows, n_cols = self.shape
+        padding = np.zeros((n_rows, dim - n_cols))
+
+        return np.hstack([self, padding])
