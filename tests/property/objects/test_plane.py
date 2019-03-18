2856215e66b5c49939077b63dedcfb78f00bd087
Andrew Hynes <andrewhynes@ymail.com>
2019-03-17 21:18:58 -0400

    Replace is_collinear with are_collinear in property tests

--- tests/property/objects/test_plane.py
+++ tests/property/objects/test_plane.py
@@ -1,13 +1,13 @@
 import pytest
 from hypothesis import given
 
-from skspatial.objects import Point, Plane
+from skspatial.objects import Points, Plane
 from tests.property.strategies import st_arrays
 
 
 @given(st_arrays, st_arrays, st_arrays)
 def test_from_points_failure(array_a, array_b, array_c):
 
-    if Point(array_a).is_collinear(array_b, array_c):
+    if Points([array_a, array_b, array_c]).are_collinear():
         with pytest.raises(Exception):
             Plane.from_points(array_a, array_b, array_c)
--- tests/property/test_measurement.py
+++ tests/property/test_measurement.py
@@ -2,7 +2,7 @@ import numpy as np
 from hypothesis import given
 
 from skspatial.measurement import area_triangle, volume_tetrahedron
-from skspatial.objects import Point
+from skspatial.objects import Point, Points
 from tests.property.strategies import st_arrays, st_line
 
 
@@ -11,7 +11,7 @@ def test_area_triangle(array_a, array_b, array_c):
 
     area = area_triangle(array_a, array_b, array_c)
 
-    assert np.isclose(area, 0) == Point(array_a).is_collinear(array_b, array_c)
+    assert np.isclose(area, 0) == Points([array_a, array_b, array_c]).are_collinear()
 
 
 @given(st_line(), st_line())
