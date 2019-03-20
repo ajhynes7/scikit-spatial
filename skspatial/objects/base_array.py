cab9bfa3dd2727c71a98da9ca9f418e4765a7bdd
Andrew Hynes <andrewhynes@ymail.com>
2019-03-20 10:39:01 -0400

    Add requirement that arrays must have length greater than one

--- skspatial/objects/base_array.py
+++ skspatial/objects/base_array.py
@@ -188,7 +188,7 @@ def norm_dim(func):
 class _BaseArray(np.ndarray):
     """Private base class for spatial objects based on a single NumPy array."""
 
-    @require("The input array must not be empty.", lambda args: len(args.array_like) > 0)
+    @require("The input array must have more than one element.", lambda args: len(args.array_like) > 1)
     @require("The input array must only contain finite numbers.", lambda args: np.all(np.isfinite(args.array_like)))
     def __new__(cls, array_like):
 
