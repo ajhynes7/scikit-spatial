fb4976e3647c6b1f113b251021bccebd7b13f2c7
Andrew Hynes <andrewhynes@ymail.com>
2019-03-24 15:34:19 -0400

    Remove exception test from plane property test
    It's already implemented by a contract.

--- tests/property/objects/test_plane.py
+++ tests/property/objects/test_plane.py
@@ -1,34 +1,18 @@
-import hypothesis.strategies as st
-import pytest
 from hypothesis import given
 
 from skspatial.objects import Point, Points, Plane
-from tests.property.strategies import st_array_fixed
+from tests.property.strategies import consistent_dim, st_array_fixed
 
 
-@given(st.data())
-def test_from_points(data):
-
-    dim = data.draw(st.sampled_from([2, 3]))
-
-    array_a = data.draw(st_array_fixed(dim))
-    array_b = data.draw(st_array_fixed(dim))
-    array_c = data.draw(st_array_fixed(dim))
-
-    arrays = [array_a, array_b, array_c]
+@given(consistent_dim(3 * [st_array_fixed], max_dim=3))
+def test_from_points(arrays):
 
     if not Points(arrays).are_collinear():
 
         # The plane must contain each point.
-        plane = Plane.from_points(array_a, array_b, array_c)
+        plane = Plane.from_points(*arrays)
 
         for array in arrays:
 
             point = Point(array).set_dimension(plane.get_dimension())
             assert plane.contains_point(point)
-
-    else:
-        # The points are collinear, so the plane is undefined.
-
-        with pytest.raises(Exception):
-            Plane.from_points(array_a, array_b, array_c)
