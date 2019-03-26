a02995c58cbacb45f62abceeb7fc4286fc28ad86
Andrew Hynes <andrewhynes@ymail.com>
2019-03-26 14:20:32 -0400

    Add tolerance to plane.from_points property test

--- tests/property/objects/test_plane.py
+++ tests/property/objects/test_plane.py
@@ -1,5 +1,6 @@
 from hypothesis import given
 
+from skspatial.constants import ATOL
 from skspatial.objects import Point, Points, Plane
 from tests.property.strategies import consistent_dim, st_array_fixed
 
@@ -7,7 +8,7 @@ from tests.property.strategies import consistent_dim, st_array_fixed
 @given(consistent_dim(3 * [st_array_fixed], max_dim=3))
 def test_from_points(arrays):
 
-    if not Points(arrays).are_collinear():
+    if not Points(arrays).are_collinear(tol=ATOL):
 
         # The plane must contain each point.
         plane = Plane.from_points(*arrays)
