ae3feef2b041300cbef0c5ffdc955f135982de78
Andrew Hynes <andrewhynes@ymail.com>
2019-03-28 11:30:58 -0400

    Edit measurement prop tests to use conditional rather than biconditional
    This reduces the risk of failing examples.

--- tests/property/test_measurement.py
+++ tests/property/test_measurement.py
@@ -11,11 +11,15 @@ from tests.property.strategies import consistent_dim, st_array_fixed
 def test_area_triangle(arrays):
 
     area = area_triangle(*arrays)
-    assert np.isclose(area, 0, atol=ATOL) == Points(arrays).are_collinear(tol=ATOL)
+
+    if np.isclose(area, 0):
+        assert Points(arrays).are_collinear(tol=ATOL)
 
 
 @given(consistent_dim(4 * [st_array_fixed], max_dim=3))
 def test_volume_tetrahedron(arrays):
 
     volume = volume_tetrahedron(*arrays)
-    assert np.isclose(volume, 0, atol=ATOL) == Points(arrays).are_coplanar(tol=ATOL)
+
+    if np.isclose(volume, 0):
+        assert Points(arrays).are_coplanar(tol=ATOL)
