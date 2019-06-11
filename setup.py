d0c499a92eb990205ddf235aec689bf592dff1b5
Andrew Hynes <andrewhynes@ymail.com>
2019-06-11 12:12:47 -0400

    Remove python 3.5 from setup.py
    This is because matplotlib requires python >= 3.6

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
