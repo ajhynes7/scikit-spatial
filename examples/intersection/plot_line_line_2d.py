4a40327214bd22f82931d1ca00028d594777a7a3
Andrew Hynes <andrewhynes@ymail.com>
2019-06-21 08:38:28 -0400

    Change plot_3d to plot_2d in 2D line intersection example

--- examples/intersection/plot_line_line_2d.py
+++ examples/intersection/plot_line_line_2d.py
@@ -6,7 +6,7 @@ Intersect two 2D lines.
 
 """
 from skspatial.objects import Line
-from skspatial.plotting import plot_3d
+from skspatial.plotting import plot_2d
 
 
 line_a = Line(point=[0, 0], direction=[1, 1.5])
@@ -15,7 +15,7 @@ line_b = Line(point=[5, 0], direction=[-1, 1])
 point_intersection = line_a.intersect_line(line_b)
 
 
-plot_3d(
+plot_2d(
     line_a.plotter(t_1=3),
     line_b.plotter(t_1=4),
     point_intersection.plotter(c='k', s=75, zorder=3),
