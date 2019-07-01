89478a7df07f1dc9d788d6c06c2041291f2ec804
Andrew Hynes <andrewhynes@ymail.com>
2019-06-30 22:17:07 -0400

    Add docstring to project_point in BaseSphere and delete other methods

--- skspatial/objects/_base_sphere.py
+++ skspatial/objects/_base_sphere.py
@@ -28,16 +28,38 @@ class _BaseSphere(_BaseComposite):
 
         return f"{name_class}(point={repr_point}, radius={self.radius})"
 
-    def diameter(self):
-
-        return 2 * self.radius
-
-    def circumference(self):
-
-        return 2 * np.pi * self.radius
-
     def project_point(self, point):
+        """
+        Project a point onto the surface of the circle or sphere.
 
+        Parameters
+        ----------
+        point : array_like
+            Input point.
+
+        Returns
+        -------
+        Point
+            Point projected onto the circle or sphere.
+
+        Examples
+        --------
+        >>> from skspatial.objects import Circle
+
+        >>> circle = Circle([0, 0], 1)
+
+        >>> circle.project_point([1, 1]).round(3)
+        array([0.707, 0.707])
+
+        >>> circle.project_point([-6, 3]).round(3)
+        array([-0.894,  0.447])
+
+        >>> from skspatial.objects import Sphere
+
+        >>> Sphere([0, 0, 0], 2).project_point([1, 2, 3]).round(3)
+        array([0.535, 1.069, 1.604])
+
+        """
         vector_to_point = Vector.from_points(self.point, point)
 
         return self.point + self.radius * vector_to_point.unit()
