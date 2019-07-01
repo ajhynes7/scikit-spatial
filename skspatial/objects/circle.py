56c1fd5afda7bc91a1638d3797b0186b91787f1c
Andrew Hynes <andrewhynes@ymail.com>
2019-06-30 22:03:04 -0400

    Add docstrings to Circle and Plane classes and methods

--- skspatial/objects/circle.py
+++ skspatial/objects/circle.py
@@ -4,32 +4,176 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from skspatial.objects._base_sphere import _BaseSphere
-from skspatial.objects.point import Point, Vector
+from skspatial.objects.point import Point
 
 
 class Circle(_BaseSphere):
+    """
+    A circle in 2D space.
+
+    The circle is defined by a 2D point and a radius.
+
+    Parameters
+    ----------
+    point : (2,) array_like
+        Center of the circle.
+    radius : {int, float}
+        Radius of the circle/
+
+    Attributes
+    ----------
+    point : (2,) Point
+        Center of the circle.
+    radius : {int, float}
+        Radius of the circle.
+    dimension : int
+        Dimension of the circle.
+
+    Raises
+    ------
+    ValueError
+        If the radius is not positive, or if the point is not 2D.
+
+    Examples
+    --------
+    >>> from skspatial.objects import Circle
+
+    >>> circle = Circle([2, 5], 3)
+
+    >>> circle
+    Circle(point=Point([2, 5]), radius=3)
+
+    >>> circle.dimension
+    2
+
+    >>> circle.area().round(2)
+    28.27
+
+    >>> Circle([0, 0, 0], 1)
+    Traceback (most recent call last):
+    ...
+    ValueError: The point must be 2D.
+
+    >>> Circle([0, 0], 0)
+    Traceback (most recent call last):
+    ...
+    ValueError: The radius must be positive.
+
+    """
 
     def __init__(self, point, radius):
 
         super().__init__(point, radius)
 
-    def diameter(self):
-
-        return 2 * self.radius
+        if self.point.dimension != 2:
+            raise ValueError("The point must be 2D.")
 
     def circumference(self):
+        r"""
+        Return the circumference of the circle.
 
-        return 2 * np.pi * self.radius
+        The circumference :math:`C` of a circle with radius :math:`r` is
+
+        .. math:: C = 2 \pi r
+
+        Returns
+        -------
+        np.float64
+            Circumference of the circle.
+
+        Examples
+        --------
+        >>> from skspatial.objects import Circle
+
+        >>> Circle([0, 0], 1).area().round(2)
+        3.14
+
+        >>> Circle([0, 0], 2).area().round(2)
+        12.57
+
+        """
+        return np.float64(2 * np.pi * self.radius)
 
     def area(self):
+        r"""
+        Return the area of the circle.
 
-        return np.pi * self.radius ** 2
+        The area :math:`A` of a circle with radius :math:`r` is
+
+        .. math:: A = \pi r ^ 2
+
+        Returns
+        -------
+        np.float64
+            Area of the circle.
+
+        Examples
+        --------
+        >>> from skspatial.objects import Circle
+
+        >>> Circle([0, 0], 1).area().round(2)
+        3.14
+
+        >>> Circle([0, 0], 2).area().round(2)
+        12.57
+
+        """
+        return np.float64(np.pi * self.radius ** 2)
 
     def intersect_line(self, line):
+        """
+        Intersect the circle with a line.
 
-        # Two points defining a line.
-        # The points on the line are translated to mimic the circle being centered on the origin.
-        vector_to_line = Vector.from_points(self.point, line.point)
+        A line intersects a circle at two points.
+
+        Parameters
+        ----------
+        line : Line
+            Input line.
+
+        Returns
+        -------
+        point_a, point_b : Point
+            The two points of intersection.
+
+        Raises
+        ------
+        ValueError
+            If the line does not intersect the circle.
+
+        References
+        ----------
+        http://mathworld.wolfram.com/Circle-LineIntersection.html
+
+        Examples
+        --------
+        >>> from skspatial.objects import Circle, Line
+
+        >>> circle = Circle([0, 0], 1)
+
+        >>> circle.intersect_line(Line(point=[0, 0], direction=[1, 0]))
+        (Point([-1.,  0.]), Point([1., 0.]))
+
+        >>> circle.intersect_line(Line(point=[0, 0], direction=[1, 1]))
+        (Point([-0.70710678, -0.70710678]), Point([0.70710678, 0.70710678]))
+
+        >>> circle.intersect_line(Line(point=[1, 2], direction=[1, 1]))
+        (Point([-1.,  0.]), Point([0., 1.]))
+
+        If the line is tangent to the circle, the two intersection points are the same.
+
+        >>> circle.intersect_line(Line(point=[1, 0], direction=[0, 1]))
+        (Point([1., 0.]), Point([1., 0.]))
+
+        The circle does not have to be centered on the origin.
+
+        >>> Circle([2, 3], 5).intersect_line(Line([1, 1], [2, 3]))
+        (Point([-0.53846154, -1.30769231]), Point([5., 7.]))
+
+        >>> circle.intersect_line(Line(point=[5, 0], direction=[1, 1]))
+        Traceback (most recent call last):
+        ...
+        ValueError: The line does not intersect the circle.
 
         """
         # Two points on the line.
@@ -71,8 +215,31 @@ class Circle(_BaseSphere):
 
         return point_a, point_b
 
-    def plot_2d(self, ax, **kwargs):
+    def plot_2d(self, ax_2d, **kwargs):
+        """
+        Plot the circle in 2D.
 
+        Parameters
+        ----------
+        ax_2d : Axes
+            Instance of :class:`~matplotlib.axes.Axes`gt.
+        kwargs : dict, optional
+            Additional keywords passed to :Class:`matplotlib.patches.Circle`.
+
+        Examples
+        --------
+        .. plot::
+            :include-source:
+
+            >>> import matplotlib.pyplot as plt
+
+            >>> from skspatial.objects import Circle
+
+            >>> fig, ax = plt.subplots()
+            >>> Circle([0, 0], 1).plot_2d(ax, fill=False)
+            >>> limits = plt.axis([-2, 2, -2, 2])
+
+        """
         circle = plt.Circle(self.point, self.radius, **kwargs)
 
-        ax.add_artist(circle)
+        ax_2d.add_artist(circle)
--- skspatial/objects/sphere.py
+++ skspatial/objects/sphere.py
@@ -7,21 +7,138 @@ from skspatial.objects.vector import Vector
 
 
 class Sphere(_BaseSphere):
+    """
+    A sphere in 3D space.
+
+    The sphere is defined by a 3D point and a radius.
+
+    Parameters
+    ----------
+    point : (3,) array_like
+        Center of the sphere.
+    radius : {int, float}
+        Radius of the sphere.
+
+    Attributes
+    ----------
+    point : (3,) Point
+        Center of the circle.
+    radius : {int, float}
+        Radius of the sphere.
+    dimension : int
+        Dimension of the sphere.
+
+    Raises
+    ------
+    ValueError
+        If the radius is not positive, or if the point is not 3D.
+
+    Examples
+    --------
+    >>> from skspatial.objects import Sphere
+
+    >>> sphere = Sphere([1, 2, 3], 5)
+
+    >>> sphere
+    Sphere(point=Point([1, 2, 3]), radius=5)
+
+    >>> sphere.dimension
+    3
+
+    >>> sphere.surface_area().round(2)
+    314.16
+
+    >>> Sphere([0, 0], 0)
+    Traceback (most recent call last):
+    ...
+    ValueError: The radius must be positive.
+
+    >>> Sphere([0, 0, 0, 0], 1)
+    Traceback (most recent call last):
+    ...
+    ValueError: The point must be 3D.
+
+    """
 
     def __init__(self, point, radius):
 
         super().__init__(point, radius)
 
-    def surface_area(self):
+        if self.point.dimension != 3:
+            raise ValueError("The point must be 3D.")
 
-        return 4 * np.pi * self.radius ** 2
+    def surface_area(self):
+        r"""
+        Return the surface area of the sphere.
+
+        The surface area :math:`A` of a sphere with radius :math:`r` is
+
+        .. math:: A = 4 \pi r ^ 2
+
+        Returns
+        -------
+        np.float64
+            Surface area of the sphere.
+
+        Examples
+        --------
+        >>> from skspatial.objects import Sphere
+
+        >>> Sphere([0, 0, 0], 1).surface_area().round(2)
+        12.57
+
+        >>> Sphere([0, 0, 0], 2).surface_area().round(2)
+        50.27
+
+        """
+        return np.float64(4 * np.pi * self.radius ** 2)
 
     def volume(self):
+        r"""
+        Return the volume of the sphere.
 
-        return 4 / 3 * np.pi * self.radius ** 3
+        The volume :math:`V` of a sphere with radius :math:`r` is
+
+        .. math:: V = \frac{4}{3} \pi r ^ 3
+
+        Returns
+        -------
+        np.float64
+            Volume of the sphere.
+
+        Examples
+        --------
+        >>> from skspatial.objects import Sphere
+
+        >>> Sphere([0, 0, 0], 1).volume().round(2)
+        4.19
+
+        >>> Sphere([0, 0, 0], 2).volume().round(2)
+        33.51
+
+        """
+        return np.float64(4 / 3 * np.pi * self.radius ** 3)
 
     def intersect_line(self, line):
+        """
+        Intersect the sphere with a line.
 
+        A line intersects a sphere at two points.
+
+        Parameters
+        ----------
+        line : Line
+            Input line.
+
+        Returns
+        -------
+        point_a, point_b : Point
+            The two points of intersection.
+
+        Examples
+        -------
+
+        """
         vector_to_line = Vector.from_points(self.point, line.point)
         vector_unit = line.direction.unit()
 
@@ -39,8 +156,32 @@ class Sphere(_BaseSphere):
 
         return point_a, point_b
 
-    def plot_3d(self, ax, **kwargs):
+    def plot_3d(self, ax_3d, **kwargs):
+        """
+        Plot the sphere in 3D.
 
+        Parameters
+        ----------
+        ax_3d : Axes3D
+            Instance of :class:`~mpl_toolkits.mplot3d.axes3d.Axes3D`.
+        kwargs : dict, optional
+            Additional keywords passed to :meth:`~mpl_toolkits.mplot3d.axes3d.Axes3D.plot_surface`.
+
+        Examples
+        --------
+        .. plot::
+            :include-source:
+
+            >>> import matplotlib.pyplot as plt
+            >>> from mpl_toolkits.mplot3d import Axes3D
+
+            >>> from skspatial.objects import Sphere
+
+            >>> fig = plt.figure()
+            >>> ax = fig.add_subplot(111, projection='3d')
+            >>> Sphere([0, 0, 0], 1).plot_3d(ax, cmap='coolwarm')
+
+        """
         angles_a = np.linspace(0, np.pi, 30)
         angles_b = np.linspace(0, 2 * np.pi, 30)
 
@@ -48,4 +189,4 @@ class Sphere(_BaseSphere):
         Y = np.outer(np.sin(angles_a), np.cos(angles_b))
         Z = np.outer(np.cos(angles_a), np.ones_like(angles_b))
 
-        ax.plot_surface(X, Y, Z, **kwargs)
+        ax_3d.plot_surface(X, Y, Z, **kwargs)
