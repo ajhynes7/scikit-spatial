58f92f71b387342510798712fd985f09f6ffa937
Andrew Hynes <andrewhynes@ymail.com>
2019-07-03 17:09:45 -0400

    Delete line/plane set_dimension tests
    These objects no longer have a set_dimension method

--- skspatial/tests/unit/objects/test_base_line_plane.py
+++ skspatial/tests/unit/objects/test_base_line_plane.py
@@ -12,22 +12,3 @@ def test_dimension(class_spatial, point, vector, dim_expected):
 
     object_spatial = class_spatial(point, vector)
     assert object_spatial.dimension == dim_expected
-
-
-@pytest.mark.parametrize("class_spatial", [Line, Plane])
-@pytest.mark.parametrize(
-    "point, vector, dim, point_expected, vector_expected",
-    [
-        ([0, 0], [1, 0], 2, [0, 0], [1, 0]),
-        ([0, 0], [1, 0], 3, [0, 0, 0], [1, 0, 0]),
-        ([0, 0], [1, 0], 5, [0, 0, 0, 0, 0], [1, 0, 0, 0, 0]),
-    ],
-)
-def test_set_dimension(
-    class_spatial, point, vector, dim, point_expected, vector_expected
-):
-
-    object_spatial = class_spatial(point, vector).set_dimension(dim)
-
-    assert object_spatial.point.is_close(point_expected)
-    assert object_spatial.vector.is_close(vector_expected)
