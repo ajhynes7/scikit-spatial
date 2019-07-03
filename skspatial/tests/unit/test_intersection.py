2dfa37e10af0769b5c746bbd079ecddc6f289878
Andrew Hynes <andrewhynes@ymail.com>
2019-07-03 09:31:27 -0400

    Add unit tests for sphere and circle/sphere intersection

--- skspatial/tests/unit/test_intersection.py
+++ skspatial/tests/unit/test_intersection.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pytest
 
-from skspatial.objects import Line, Plane
+from skspatial.objects import Line, Plane, Circle, Sphere
 
 
 @pytest.mark.parametrize(
@@ -103,3 +104,72 @@ def test_intersect_planes_failure(plane_a, plane_b):
 
     with pytest.raises(Exception):
         plane_a.intersect_plane(plane_b)
+
+
+@pytest.mark.parametrize(
+    "circle, line, point_a_expected, point_b_expected",
+    [
+        (Circle([0, 0], 1), Line([0, 0], [1, 0]), [-1, 0], [1, 0]),
+        (Circle([0, 0], 1), Line([0, 0], [0, 1]), [0, -1], [0, 1]),
+        (Circle([0, 0], 1), Line([0, 1], [1, 0]), [0, 1], [0, 1]),
+        (Circle([0, 0], 1), Line([0, 0.5], [1, 0]), [-np.sqrt(3) / 2, 0.5], [np.sqrt(3) / 2, 0.5]),
+        (Circle([1, 0], 1), Line([0, 0], [1, 0]), [0, 0], [2, 0]),
+    ],
+)
+def test_intersect_circle_line(circle, line, point_a_expected, point_b_expected):
+
+    point_a, point_b = circle.intersect_line(line)
+
+    assert point_a.is_close(point_a_expected)
+    assert point_b.is_close(point_b_expected)
+
+
+@pytest.mark.parametrize(
+    "circle, line",
+    [
+        # The circle does not intersect the line.
+        (Circle([0, 0], 1), Line([0, 2], [1, 0])),
+        (Circle([0, 0], 1), Line([0, -2], [1, 0])),
+        (Circle([0, 0], 1), Line([2, 0], [0, 1])),
+        (Circle([0, 0], 1), Line([3, 0], [1, 1])),
+        (Circle([1.5, 0], 1), Line([0, 0], [1, 0])),
+    ],
+)
+def test_intersect_circle_line_failure(circle, line):
+
+    with pytest.raises(Exception):
+        circle.intersect_line(line)
+
+
+@pytest.mark.parametrize(
+    "sphere, line, point_a_expected, point_b_expected",
+    [
+        (Sphere([0, 0, 0], 1), Line([0, 0, 0], [1, 0, 0]), [-1, 0, 0], [1, 0, 0]),
+        (Sphere([0, 0, 0], 1), Line([0, 0, 0], [1, 1, 0]), -np.sqrt(2) / 2 * np.array([1, 1, 0]), np.sqrt(2) / 2 * np.array([1, 1, 0])),
+        (Sphere([0, 0, 0], 1), Line([0, 0, 0], [1, 1, 1]), -np.sqrt(3) / 3 * np.ones(3), np.sqrt(3) / 3 * np.ones(3)),
+        (Sphere([1, 0, 0], 1), Line([0, 0, 0], [1, 0, 0]), [0, 0, 0], [2, 0, 0]),
+        (Sphere([0, 0, 0], 1), Line([1, 0, 0], [0, 0, 1]), [1, 0, 0], [1, 0, 0])
+    ],
+)
+def test_intersect_sphere_line(sphere, line, point_a_expected, point_b_expected):
+
+    point_a, point_b = sphere.intersect_line(line)
+
+    assert point_a.is_close(point_a_expected)
+    assert point_b.is_close(point_b_expected)
+
+
+@pytest.mark.parametrize(
+    "sphere, line",
+    [
+        (Sphere([0, 0, 0], 1), Line([0, 0, 2], [1, 0, 0])),
+        (Sphere([0, 0, 0], 1), Line([0, 0, -2], [1, 0, 0])),
+        (Sphere([0, 2, 0], 1), Line([0, 0, 0], [1, 0, 0])),
+        (Sphere([0, -2, 0], 1), Line([0, 0, 0], [1, 0, 0])),
+        (Sphere([5, 0, 0], 1), Line([0, 0, 0], [1, 1, 1])),
+    ],
+)
+def test_intersect_sphere_line_failure(sphere, line):
+
+    with pytest.raises(Exception):
+        sphere.intersect_line(line)
