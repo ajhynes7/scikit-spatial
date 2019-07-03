ccfd754be5084945823af76df1108a18a33ba660
Andrew Hynes <andrewhynes@ymail.com>
2019-07-03 17:08:59 -0400

    Split line_plane failure tests to test for separate ValueError messages

--- skspatial/tests/unit/objects/test_line_plane.py
+++ skspatial/tests/unit/objects/test_line_plane.py
@@ -9,14 +9,28 @@ from skspatial.objects import Line, Plane
 @pytest.mark.parametrize(
     "point, vector",
     [
-        # The zero vector cannot be used.
         ([0, 0], [0, 0]),
         ([1, 1], [0, 0]),
         ([1, 1, 1], [0, 0, 0]),
         ([4, 5, 2, 3], [0, 0, 0, 0]),
     ],
 )
-def test_init_failure(class_spatial, point, vector):
+def test_zero_vector_failure(class_spatial, point, vector):
 
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError, match="The vector must not be the zero vector."):
+        class_spatial(point, vector)
+
+
+@pytest.mark.parametrize("class_spatial", [Line, Plane])
+@pytest.mark.parametrize(
+    "point, vector",
+    [
+        ([0, 0, 1], [1, 1]),
+        ([0, 0], [1]),
+        ([1], [0, 1]),
+    ],
+)
+def test_dimension_failure(class_spatial, point, vector):
+
+    with pytest.raises(ValueError, match="The point and vector must have the same dimension."):
         class_spatial(point, vector)
