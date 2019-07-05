870ade3482d361bc91cfbb6ee55b023f85e07900
Andrew Hynes <andrewhynes@ymail.com>
2019-07-05 13:20:02 -0400

    Format code with black

--- skspatial/tests/property/objects/test_line.py
+++ skspatial/tests/property/objects/test_line.py
@@ -32,7 +32,9 @@ def test_two_lines(lines):
         # The lines are parallel, so they must be coplanar.
         assert line_a.is_coplanar(line_b)
 
-    if line_a.is_coplanar(line_b, tol=0) and not line_a.direction.is_parallel(line_b.direction, atol=ATOL):
+    if line_a.is_coplanar(line_b, tol=0) and not line_a.direction.is_parallel(
+        line_b.direction, atol=ATOL
+    ):
         # The lines are coplanar but not parallel, so they must intersect.
         distance = line_a.distance_line(line_b)
         assert np.isclose(distance, 0)
