42d08f50df9dedf788b2a359442f37a1deab7b76
Andrew Hynes <andrewhynes@ymail.com>
2019-07-14 12:39:00 -0400

    Add unit tests for Sphere.to_points

--- skspatial/tests/unit/test_plotting.py
+++ skspatial/tests/unit/test_plotting.py
@@ -1,6 +1,6 @@
 import pytest
 
-from skspatial.objects import Plane
+from skspatial.objects import Plane, Sphere
 
 
 @pytest.mark.parametrize(
@@ -18,5 +18,23 @@ from skspatial.objects import Plane
 def test_plane_points(plane, points_expected):
 
     points = plane.to_points()
+
+    assert points.is_close(points_expected)
+
+
+@pytest.mark.parametrize(
+    "sphere, n_angles, points_expected",
+    [
+        (Sphere([0, 0, 0], 1), 1, [[0, 0, 1]]),
+        (Sphere([0, 0, 0], 1), 2, [[0, 0, -1], [0, 0, 1]]),
+        (Sphere([0, 0, 0], 1), 3, [[0, -1, 0], [0, 0, -1], [0, 0, 1], [0, 1, 0]]),
+        (Sphere([0, 0, 0], 2), 3, [[0, -2, 0], [0, 0, -2], [0, 0, 2], [0, 2, 0]]),
+        (Sphere([1, 0, 0], 1), 3, [[1, -1, 0], [1, 0, -1], [1, 0, 1], [1, 1, 0]]),
+        (Sphere([1, 1, 1], 1), 3, [[1, 0, 1], [1, 1, 0], [1, 1, 2], [1, 2, 1]]),
+    ],
+)
+def test_sphere_points(sphere, n_angles, points_expected):
+
+    points = sphere.to_points(n_angles).round(3).unique()
 
     assert points.is_close(points_expected)
