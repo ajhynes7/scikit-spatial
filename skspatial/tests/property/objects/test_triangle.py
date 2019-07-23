c0ddc7a11bb27f933ec51f6b2a0dbd249300b9e5
Andrew Hynes <andrewhynes@ymail.com>
2019-07-23 19:57:45 -0400

    Add property test for Triangle.orthocenter()

---
+++ skspatial/tests/property/objects/test_triangle.py
@@ -0,0 +1,15 @@
+from hypothesis import given
+
+from ..strategies import triangles
+
+
+@given(triangles(2))
+def test_orthocenter(triangle):
+
+    point_orthocenter = triangle.orthocenter()
+
+    alt_a = triangle.altitude('A')
+    alt_b = triangle.altitude('B')
+    alt_c = triangle.altitude('C')
+
+    assert all(alt.contains_point(point_orthocenter, abs_tol=1e-3) for alt in [alt_a, alt_b, alt_c])
