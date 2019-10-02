f586d79da44b01a76a899d39c5d78db341bc90ae
Andrew Hynes <andrewjhynes@gmail.com>
2019-10-02 17:33:43 -0400

    Add property test for Points.normalize_distance

--- skspatial/tests/property/test_transformation.py
+++ skspatial/tests/property/test_transformation.py
@@ -1,16 +1,16 @@
 import hypothesis.strategies as st
 import numpy as np
-from hypothesis import given
+from hypothesis import assume, given
 from numpy.testing import assert_array_almost_equal
 
-from skspatial.objects import Points
-from skspatial.tests.property.strategies import arrays_fixed, lines, multi_points
+from .strategies import lines, multi_points
+from .constants import ATOL
 
 
 @given(st.data())
 def test_mean_center(data):
 
-    dim = data.draw(st.integers(min_value=2, max_value=4))
+    dim = data.draw(st.integers(min_value=2, max_value=5))
     points = data.draw(multi_points(dim))
 
     points_centered = points.mean_center()
@@ -20,6 +20,20 @@ def test_mean_center(data):
     origin = np.zeros(points.dimension)
 
     assert_array_almost_equal(centroid_new, origin)
+
+
+@given(st.data())
+def test_normalize_distance(data):
+
+    dim = data.draw(st.integers(min_value=2, max_value=5))
+    points = data.draw(multi_points(dim))
+
+    assume(not np.all(points == 0))
+
+    points_transformed = points.normalize_distance()
+    distances_to_points = np.linalg.norm(points_transformed, axis=1)
+
+    assert np.logical_and(distances_to_points >= 0, distances_to_points <= 1 + ATOL).all()
 
 
 @given(st.data())
