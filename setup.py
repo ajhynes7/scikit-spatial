36f9223a3f7aa7b6969c25e11d6a8e55065cbaaa
Andrew Hynes <andrewjhynes@gmail.com>
2020-02-01 17:07:43 -0500

    Remove python 3.6 from setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering',
     ],
 
