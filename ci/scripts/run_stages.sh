10c58db2aae384636b2dfa3969b7963e73cafb5a
Andrew Hynes <andrewjhynes@gmail.com>
2020-12-06 21:51:37 -0500

    Fix syntax of for loop

--- ci/scripts/run_stages.sh
+++ ci/scripts/run_stages.sh
@@ -1,5 +1,5 @@
 python_version="$1"
 
-for stage in "lint_code" "lint_docs" "types" "readme" "doctests" "unit" "property" "docs"
+for stage in "lint_code" "lint_docs" "types" "readme" "doctests" "unit" "property" "docs"; do
     bash $(pwd)/ci/scripts/run_stage.sh $stage $TRAVIS_PYTHON_VERSION
 done
