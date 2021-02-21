ebe4591d16346834cc06f393889a9732e39c2a1f
Andrew Hynes <andrewjhynes@gmail.com>
2021-02-21 17:37:27 -0330

    Fix: Revert back to version 5.2.0 in `setup.py`
    I'll change it to 6.0.0 later when I'm doing a release.

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     include_package_data=True,
     license="BSD license",
     url='https://github.com/ajhynes7/scikit-spatial',
-    version='6.0.0',
+    version='5.2.0',
     zip_safe=False,
 )
