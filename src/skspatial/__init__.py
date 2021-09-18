b12bc4c78248e3827115d2fbb0f8a8b008a752bc
Andrew Hynes <andrewjhynes@gmail.com>
2021-09-18 18:41:37 -0400

    types: ignore type of version-dependent import

--- src/skspatial/__init__.py
+++ src/skspatial/__init__.py
@@ -7,6 +7,6 @@ try:
     import importlib.metadata as importlib_metadata
 
 except ModuleNotFoundError:
-    import importlib_metadata
+    import importlib_metadata  # type: ignore
 
 __version__ = importlib_metadata.version("scikit-spatial")
