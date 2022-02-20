9b3d49a936ef97d7b0b30754859db195f3e72987
cristiano.pizzamiglio <cristiano.pizzamiglio@limacorporate.com>
2022-02-21 00:43:58 +0100

    Add unit tests for the area_signed function in test_measurement.py

--- tests/unit/test_measurement.py
+++ tests/unit/test_measurement.py
@@ -3,8 +3,10 @@ import math
 import numpy as np
 import pytest
 
+from skspatial.objects import Points
 from skspatial.measurement import area_triangle
 from skspatial.measurement import volume_tetrahedron
+from skspatial.measurement import area_signed
 
 
 @pytest.mark.parametrize(
@@ -41,3 +43,49 @@ def test_volume_tetrahedron(array_a, array_b, array_c, array_d, volume_expected)
 
     volume = volume_tetrahedron(array_a, array_b, array_c, array_d)
     assert math.isclose(volume, volume_expected)
+
+
+@pytest.mark.parametrize(
+    ("points", "area_expected"),
+    [
+        # Counter-clockwise triangle
+        ([[2, 2], [6, 2], [4, 5]], 6),
+
+        # Clockwise triangle
+        ([[1, 3], [-4, 3], [-3, 4]], -2.5),
+
+        # Counter-clockwise square
+        ([[-1, 2], [2, 5], [-1, 8], [-4, 5]], 18),
+
+        # Clockwise irregular convex pentagon
+        ([[-2, 2], [-5, 2], [-8, 5], [-4, 8], [-1, 5]], -25.5),
+
+        # Counter-clockwise irregular convex hexagon
+        ([[3, -2], [6, -3], [10, -1], [8, 4], [4, 3], [1, 1]], 39.5),
+
+        # Clockwise non-convex polygon
+        ([[5, -2], [1, -1], [0, 4], [6, 6], [3, 3]], -22),
+
+        # Self-overlapping polygon
+        ([[-4, 4], [-4, 1], [2, 4], [2, 1]], 0)
+    ],
+)
+def test_area_signed(points, area_expected):
+
+    points = Points(points)
+    area = area_signed(points)
+
+    assert area == area_expected
+
+
+@pytest.mark.parametrize(
+    ("points", "message_expected"),
+    [
+        ([[1, 0, 0], [-1, 0, 0], [0, 1, 0]], "The points must be 2D."),
+        ([[2, 0], [-2, 0]], "There must be at least 3 points."),
+    ],
+)
+def test_area_signed_failure(points, message_expected):
+
+    with pytest.raises(ValueError, match=message_expected):
+        area_signed(points)
