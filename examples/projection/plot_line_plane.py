621b8f1378d6acf29079a85c34b279b8278abcd8
cristiano.pizzamiglio <cristianopizzamiglio@gmail.com>
2022-04-02 01:58:33 +0200

    Add plot_line_plane.py to examples/projection

---
+++ examples/projection/plot_line_plane.py
@@ -0,0 +1,24 @@
+"""
+Vector-Plane Projection
+=======================
+
+Project a vector onto a plane.
+
+"""
+from skspatial.objects import Plane
+from skspatial.objects import Line
+from skspatial.plotting import plot_3d
+
+plane = Plane([0, 1, 0], [0, 1, 0])
+line = Line([0, -1, 0], [1, -2, 0])
+
+line_projected = plane.project_line(line)
+
+
+_, ax = plot_3d(
+    plane.plotter(lims_x=(-5, 5), lims_y=(-5, 5), alpha=0.3),
+    line.plotter(t_1=-2, t_2=2, color='k'),
+    line_projected.plotter(t_1=-2, t_2=2, color='r', linewidth=2, zorder=3),
+)
+
+ax.set_zlim([-1, 1])
