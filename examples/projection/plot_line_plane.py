4f5ce5609d51b4d2810817f857ea062095406fd0
Andrew Hynes <andrewjhynes@gmail.com>
2022-04-03 15:56:34 -0400

    simplify gallery example

--- examples/projection/plot_line_plane.py
+++ examples/projection/plot_line_plane.py
@@ -15,10 +15,8 @@ line = Line([0, -1, 0], [1, -2, 0])
 line_projected = plane.project_line(line)
 
 
-_, ax = plot_3d(
+plot_3d(
     plane.plotter(lims_x=(-5, 5), lims_y=(-5, 5), alpha=0.3),
     line.plotter(t_1=-2, t_2=2, color='k'),
-    line_projected.plotter(t_1=-2, t_2=2, color='r', linewidth=2, zorder=3),
+    line_projected.plotter(t_1=-2, t_2=4, color='r'),
 )
-
-ax.set_zlim([-1, 1])
