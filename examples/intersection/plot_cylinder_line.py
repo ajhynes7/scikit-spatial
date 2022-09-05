09ebbf660fa90c82f64c538257142b8dad4ad6ff
Andrew Hynes <andrewjhynes@gmail.com>
2022-09-05 17:29:14 -0230

    docs: add cylinder-line intersection to gallery

---
+++ examples/intersection/plot_cylinder_line.py
@@ -0,0 +1,22 @@
+"""
+Cylinder-Line Intersection
+========================
+
+"""
+from skspatial.objects import Line, Cylinder
+from skspatial.plotting import plot_3d
+
+
+cylinder = Cylinder([0, 0, 0], [0, 0, 1], 1)
+line = Line([0, 0, 0], [1, 0, 0.7])
+
+point_a, point_b = cylinder.intersect_line(line, infinite=False)
+
+
+plot_3d(
+    line.plotter(c='k'),
+    cylinder.plotter(alpha=0.2),
+    point_a.plotter(c='r', s=100),
+    point_b.plotter(c='r', s=100),
+)
+
