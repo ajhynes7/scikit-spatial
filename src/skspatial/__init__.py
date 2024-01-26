d2cdb6de189825d0050dd06802ef9a17b1fe28c8
Louis Lac <lac.louis5@gmail.com>
2024-01-26 18:18:30 +0100

    Python 3.12 support (#342)

--- src/skspatial/__init__.py
+++ src/skspatial/__init__.py
@@ -1,12 +1,7 @@
 """Top-level package for scikit-spatial."""
 
+import importlib.metadata as importlib_metadata  # type: ignore
+
 __author__ = "Andrew Hynes"
 __email__ = "andrewjhynes@gmail.com"
-
-try:
-    import importlib.metadata as importlib_metadata  # type: ignore
-
-except ModuleNotFoundError:
-    import importlib_metadata  # type: ignore
-
 __version__ = importlib_metadata.version("scikit-spatial")
