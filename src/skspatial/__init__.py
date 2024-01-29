10928ca9cafaffc65b33c1e6ca71481af08d1cfe
Andrew Hynes <andrew.hynes@colabsoftware.com>
2024-01-29 09:10:22 -0330

    fix: restore importlib_metadata

--- src/skspatial/__init__.py
+++ src/skspatial/__init__.py
@@ -1,7 +1,12 @@
 """Top-level package for scikit-spatial."""
 
-import importlib.metadata as importlib_metadata  # type: ignore
-
 __author__ = "Andrew Hynes"
 __email__ = "andrewjhynes@gmail.com"
+
+try:
+    import importlib.metadata as importlib_metadata  # type: ignore
+
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore
+
 __version__ = importlib_metadata.version("scikit-spatial")
